fdc9e1aa937f022360b9a892530e0e24fe810096
Christian <hi@lindeneg.org>
2021-04-24 10:41:23 +0200

    remove lib

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as default, getInput } from './form.hook';
-export type { UseForm, FormState, FormValueType } from './form.hook';
-export type { ValidationType, CustomValidationRule, ValidationValue, ValidationFunc, Validator } from './form.validation';
