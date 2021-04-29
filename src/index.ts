0123227be544d98f69a3450ca062dbb185edba82
Christian <hi@lindeneg.org>
2021-04-29 03:31:26 +0200

    export more hook types for external use

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as default, getInput } from './form.hook';
 
-export type { UseForm, FormState, FormValueType } from './form.hook';
+export type { UseForm, FormState, FormValueType, FormEntryConstraint, Inputs, GetInputOptions } from './form.hook';
 export type {
     ValidationType,
     CustomValidationRule,
