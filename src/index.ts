e88a94f07c192c16befd8cb678c734f9a6884e9e
Christian <hi@lindeneg.org>
2021-05-03 18:43:09 +0200

    export validate function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 export { default as default, getInput } from './form.hook';
 
-export type { UseForm, FormState, FormValueType, FormEntryConstraint, Inputs, GetInputOptions } from './form.hook';
+export { validate } from './form.validation';
+export type {
+    UseForm,
+    FormState,
+    FormValueType,
+    FormEntryConstraint,
+    Inputs,
+    GetInputOptions
+} from './form.hook';
 export type {
     ValidationType,
     CustomValidationRule,
