40111b5ac6a5f5550ef9d4568ddd06176537524a
Christian <dev@webalyze.net>
2021-09-08 17:09:21 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export { default as useForm, getInput } from "./form.hook";
-export { validate } from "./form.validation";
-
-export type { GetInputOptions } from "./form.hook";
+export { validate, validateState, getValidator } from "./form.validation";
+export type { GetInputOptions, FormElementConstraint } from "./form.hook";
 export type {
   ValidationType,
   CustomValidationRule,
   ValidationValue,
+  InputValueType,
+  FormEntryState,
   Validator,
   FormState,
   FormEntryConstraint,
